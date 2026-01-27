13dcb08bb780e8dc80027c94522a10858365524d
Namide <damien@doussaud.fr>
2026-01-27 12:39:11 +0100

    Update design

---
+++ src/components/Info.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useTemplateRef } from 'vue'
+
+const modal = useTemplateRef<HTMLDialogElement>('modal')
+</script>
+
+<template>
+  <div>
+    <button class="btn" @click="modal?.showModal()">C'est quoi ?</button>
+    <dialog ref="modal" class="modal">
+      <div class="modal-box">
+        <form method="dialog ">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            @click="modal?.close(); $event.preventDefault()">✕</button>
+        </form>
+        <h2 class="text-lg font-bold">Infos</h2>
+        <div class="py-4">
+          <p class="text-balance">C'est un outil pour entrainer les enfants à se représenter afin de leur rendre par la
+            suite les calculs plus intuitif.
+          </p>
+          <p class="text-balance mt-4">Il faut jouer à plusieurs en plaçant l'écran visible pour tous. Il faut être le
+            premier à
+            donner le nombre avant que la solution ne s'affiche.
+          </p>
+          <p class="text-balance mt-4">Si vous souhaitez donner votre avis ou participer au développement rendez-vous
+            sur
+            :<br>
+            <a href="https://github.com/Namide/constellation-nombres" rel="noreferrer noopener" target="_blank"
+              class="btn btn-link">github.com/Namide/constellation-nombres</a>
+          </p>
+        </div>
+      </div>
+    </dialog>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 import Settings from '@/components/Settings.vue';
-
+import Info from '@/components/Info.vue';
 </script>
 
 <template>
   <main
     class="min-h-dvh flex flex-col justify-center items-center w-full h-full text-center pt-5 pb-20 max-w-10/12 mx-auto">
-    <h1 class="text-5xl mb-4 text-balance">Constellation des nombres</h1>
-    <p class="text-balance">Outil pour entrainer les enfants à se représenter les nombres sous forme de
-      constellation afin de leur rendre par la suite les calculs plus intuitif.
-    </p>
-    <p class="text-balance mt-4">Jouez à plusieurs en plaçant l'écran visible pour tous. Le premier qui donne le bon
-      nombre avant l'affichage de la solution gagne 1 point.
-    </p>
-
+    <h1 class="text-5xl mb-4 text-balance">Constellation<br>des nombres</h1>
     <div class="pt-10 flex flex-col gap-5 items-center">
       <RouterLink :to="{ name: 'game' }" class="btn btn-primary btn-wide btn-xl">Commencer</RouterLink>
       <Settings />
     </div>
+    <Info class="mt-10"></Info>
   </main>
 </template>
 
